2b5bb8e965d9e88ae4928be5a60538a1ee622423
cc51758902 <72447996+cc51758902@users.noreply.github.com>
2022-02-28 12:55:37 +0800

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -9,7 +9,7 @@ rm -f get-docker.sh
 
 systemctl enable --now docker
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 mkdir -p /data/danted
